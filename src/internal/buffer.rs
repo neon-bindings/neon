888675f76a4f32baf61cf8766504da0a550280e5
Dave Herman <dherman@mozilla.com>
2015-12-21 11:57:50 -0800

    bump both crates to 0.1

--- src/internal/buffer.rs
+++ src/internal/buffer.rs
@@ -16,45 +16,10 @@ use neon_sys::buf::Buf;
 #[derive(Clone, Copy)]
 pub struct Buffer(raw::Local);
 
-/*
-impl Index<usize> for Buffer {
-    type Output = u8;
-    fn index<'a>(&'a self, index: usize) -> &'a u8 {
-        self.data().as_slice().unwrap().index(index)
-    }
-}
-
-impl IndexMut<usize> for Buffer {
-    fn index_mut<'a>(&'a mut self, index: usize) -> &mut u8 {
-        self.data().as_mut_slice().unwrap().index_mut(index)
-    }
-}
- */
-
 impl Buffer {
     pub fn new<'a, T: Scope<'a>>(_: &mut T, size: u32) -> Result<Handle<'a, SomeObject>, Throw> {
         build(|out| { unsafe { NeonSys_NewBuffer(out, size) } })
     }
-
-    /*
-    pub fn data(&self) -> Buf {
-        unsafe {
-            let mut result = Buf::uninitialized();
-            NeonSys_Buffer_Data(&mut result, self.to_raw());
-            result
-        }
-    }
-
-    pub fn as_str(&self) -> Result<&str, Utf8Error> {
-        str::from_utf8(self.data().as_slice().unwrap())
-    }
-
-    pub fn check_str(&self) -> Result<&str, Throw> {
-        self.as_str().map_err(|_| {
-            TypeError::throw::<()>("buffer contents are invalid UTF-8").err().unwrap()
-        })
-    }
-    */
 }
 
 impl AnyInternal for Buffer {
