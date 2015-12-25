3164f482913b7ca44a6a55aa6cfbe5baef5a9671
Martin Muñoz <im.mmun@gmail.com>
2015-12-25 04:55:14 -0500

    Class things up :tophat:

--- src/addon.js
+++ src/addon.js
@@ -6,14 +6,16 @@ const TEMPLATES_DIR = path.resolve(path.resolve(__dirname, ".."), "templates");
 
 const TEMPLATE = handlebars.compile(fs.readFileSync(path.resolve(TEMPLATES_DIR, "binding.cc.hbs"), 'utf8'), { noEscape: true });
 
-function Addon(project) {
-  this.project = project;
-  this.context = { project: { name: project.libName } };
-}
+class Addon {
+  constructor(project) {
+    this.project = project;
+    this.context = { project: { name: project.libName } };
+  }
 
-Addon.prototype.generate = function generate(filename) {
-  fs.writeFileSync(filename, TEMPLATE(this.context));
-};
+  generate(filename) {
+    fs.writeFileSync(filename, TEMPLATE(this.context));
+  }
+}
 
 export default function(build) {
   return new Addon(build);
--- src/build.js
+++ src/build.js
@@ -9,56 +9,58 @@ const TEMPLATES_DIR = path.resolve(path.resolve(__dirname, ".."), "templates");
 
 const TEMPLATE = handlebars.compile(fs.readFileSync(path.resolve(TEMPLATES_DIR, "binding.gyp.hbs"), 'utf8'), { noEscape: true });
 
-function Build(project, rust, type) {
-  this.project = project;
-  this.rust = rust;
-  this.type = type;
-}
+class Build {
+  constructor(project, rust, type) {
+    this.project = project;
+    this.rust = rust;
+    this.type = type;
+  }
 
-Build.prototype.generateGypfile = function generateGypfile() {
-  let release = this.type === 'release';
+  generateGypfile() {
+    let release = this.type === 'release';
 
-  let context = {
-    project: {
-      name: this.project.libName,
-      rust: { inputs: this.project.getRustInputs() }
-    },
-    build: {
-      cfg: {
-        gyp: release ? "Release" : "Debug",
-        cargo: this.type
+    let context = {
+      project: {
+        name: this.project.libName,
+        rust: { inputs: this.project.getRustInputs() }
       },
-      release: release,
-      cargo: { cmd: this.rust === 'default' ? [] : ["multirust", "run", this.rust] }
+      build: {
+        cfg: {
+          gyp: release ? "Release" : "Debug",
+          cargo: this.type
+        },
+        release: release,
+        cargo: { cmd: this.rust === 'default' ? [] : ["multirust", "run", this.rust] }
+      }
+    };
+
+    fs.writeFileSync(this.project.getGypfilePath(), TEMPLATE(context));
+  }
+
+  isStale() {
+    try {
+      let gypfile = this.project.getGypfileContents();
+      // FIXME: check if the set of .rs source files has changed?
+      return (gypfile.target_defaults.default_configuration.toLowerCase() !== this.type) ||
+             (gypfile.targets[0].target_name !== this.project.libName);
+    } catch (e) {
+      return true;
     }
-  };
-
-  fs.writeFileSync(this.project.getGypfilePath(), TEMPLATE(context));
-};
-
-Build.prototype.isStale = function isStale() {
-  try {
-    let gypfile = this.project.getGypfileContents();
-    // FIXME: check if the set of .rs source files has changed?
-    return (gypfile.target_defaults.default_configuration.toLowerCase() !== this.type) ||
-           (gypfile.targets[0].target_name !== this.project.libName);
-  } catch (e) {
-    return true;
+    return false;
   }
-  return false;
-};
 
-Build.prototype.run = function run() {
-  // 1. Generate the gypfile
-  this.generateGypfile();
+  run() {
+    // 1. Generate the gypfile
+    this.generateGypfile();
 
-  // 2. `node-gyp rebuild`
-  let options = { cwd: this.project.root, stdio: 'inherit' };
-  let result = child.spawnSync(NODE_GYP, ["rebuild"], options);
-  if (result.status !== 0) {
-    process.exit(result.status);
+    // 2. `node-gyp rebuild`
+    let options = { cwd: this.project.root, stdio: 'inherit' };
+    let result = child.spawnSync(NODE_GYP, ["rebuild"], options);
+    if (result.status !== 0) {
+      process.exit(result.status);
+    }
   }
-};
+}
 
 export default function(project, rust, type) {
   return new Build(project, rust, type);
