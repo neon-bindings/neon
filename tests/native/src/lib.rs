58d41e62e54f267dd0d9bf16338b5e7340fc7ec1
Mike Piccolo <mfpiccolo@gmail.com>
2016-05-02 23:32:18 -0700

    Knew it!  ಠ_ಠ  Fix some stuff

--- tests/native/src/lib.rs
+++ tests/native/src/lib.rs
@@ -14,17 +14,18 @@ use js::arrays::*;
 use js::objects::*;
 
 register_module!(m, {
-    m.export("return_js_string", return_js_string);
+    try!(m.export("return_js_string", return_js_string));
 
-    m.export("return_js_integer", return_js_integer);
+    try!(m.export("return_js_integer", return_js_integer));
 
-    m.export("return_js_array", return_js_array);
-    m.export("return_js_array_with_integer", return_js_array_with_integer);
-    m.export("return_js_array_with_string", return_js_array_with_string);
+    try!(m.export("return_js_array", return_js_array));
+    try!(m.export("return_js_array_with_integer", return_js_array_with_integer));
+    try!(m.export("return_js_array_with_string", return_js_array_with_string));
 
-    m.export("return_js_object", return_js_object);
-    m.export("return_js_object_with_integer", return_js_object_with_integer);
-    m.export("return_js_object_with_string", return_js_object_with_string)
+    try!(m.export("return_js_object", return_js_object));
+    try!(m.export("return_js_object_with_integer", return_js_object_with_integer));
+    try!(m.export("return_js_object_with_string", return_js_object_with_string));
+    Ok(())
 });
 
 
