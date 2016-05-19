5ee0ce75681a83d1eb0b2067a55d0b17c87814d9
Mike Piccolo <mfpiccolo@gmail.com>
2016-05-19 08:58:01 -0700

    Update tests for getter function

--- tests/lib/classes.js
+++ tests/lib/classes.js
@@ -9,12 +9,15 @@ describe('JsClass', function() {
 
   it('return a JsClass built in Rust', function () {
     u = new User(1, "some", "thing", "else");
+    assert(u instanceof User);
+  });
 
+  it('can use getter funtion defined in Rust' function () {
+    u = new User(1, "some", "thing", "else");
     assert.equal(u.get('id'), 1);
     assert.equal(u.get('first_name'), "some");
     assert.equal(u.get('last_name'), "thing");
     assert.equal(u.get('email'), "else");
-    assert.throw(function() { u.get('not_a_field') }, TypeError)
-  });
-
+    assert.throw(function() { u.get('not_a_field') }, TypeError);
+  })
 });
