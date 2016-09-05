51bd3cd450e0e5663504e3a12ad32b37479c8246
Edward Shaw <edwardshaw9@gmail.com>
2016-09-05 03:42:47 +0100

    Take module version from node headers.
    If module abi version isn't specified in env, take it from the included node headers.

--- crates/neon-sys/src/module.rs
+++ crates/neon-sys/src/module.rs
@@ -6,4 +6,7 @@ extern "system" {
     #[link_name = "NeonSys_Module_ExecKernel"]
     pub fn exec_kernel(kernel: *mut c_void, callback: extern fn(*mut c_void, *mut c_void, *mut c_void), exports: Local, scope: *mut c_void);
 
+    #[link_name = "NeonSys_Module_GetVersion"]
+    pub fn get_version() -> i32;
+
 }
