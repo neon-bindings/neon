4e10dbfec1c5b5715130a34823d2166806dcd43f
Max Brunsfeld <maxbrunsfeld@gmail.com>
2016-10-24 15:02:21 -0700

    Fix npm path on windows

--- crates/neon-sys/build.rs
+++ crates/neon-sys/build.rs
@@ -14,7 +14,7 @@ fn mode() -> &'static str {
     if debug() { "Debug" } else { "Release" }
 }
 
-#[cfg(not(windows))]
+#[cfg(unix)]
 fn object_path(libname: &str) -> String {
     format!("build/{}/obj.target/{}/src/{}.o", mode(), libname, libname)
 }
@@ -24,16 +24,22 @@ fn object_path(libname: &str) -> String {
     format!("build\\{}\\obj\\{}\\{}.obj", mode(), libname, libname)
 }
 
+#[cfg(unix)]
+const NPM_COMMAND : &'static str = "npm";
+
+#[cfg(windows)]
+const NPM_COMMAND : &'static str = "npm.cmd";
+
 fn build_object_file() {
     // Ensure that all package.json dependencies and dev dependencies are installed.
-    Command::new("npm").arg("install").status().ok()
+    Command::new(NPM_COMMAND).arg("install").status().ok()
         .expect(r#"failed to run "npm install" for neon-sys"#);
 
     // Run the package.json `configure` script, which invokes `node-gyp configure` from the local node_modules.
-    Command::new("npm").arg("run").arg(if debug() { "configure-debug" } else { "configure-release" }).status().ok().unwrap();
+    Command::new(NPM_COMMAND).arg("run").arg(if debug() { "configure-debug" } else { "configure-release" }).status().ok().unwrap();
 
     // Run the package.json `build` script, which invokes `node-gyp build` from the local node_modules.
-    Command::new("npm").arg("run").arg(if debug() { "build-debug" } else { "build-release" }).status().ok().unwrap();
+    Command::new(NPM_COMMAND).arg("run").arg(if debug() { "build-debug" } else { "build-release" }).status().ok().unwrap();
 }
 
 fn link_library() {
