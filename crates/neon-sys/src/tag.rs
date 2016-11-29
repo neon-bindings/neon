5a4f23b8984a6355014138fb162db360be4be013
jedireza <reza@akhavan.me>
2016-11-29 13:51:02 -0800

    expand neon_sys::tag mod documentation

--- crates/neon-sys/src/tag.rs
+++ crates/neon-sys/src/tag.rs
@@ -1,3 +1,5 @@
+//! Facilities for identifying the type of a `Local` handle.
+
 use raw::Local;
 
 // analog C enum `tag_t` defined in neon.h
@@ -18,39 +20,51 @@ pub enum Tag {
 
 extern "system" {
 
+    /// Returns the `Tag` of the value provided.
     #[link_name = "NeonSys_Tag_Of"]
     pub fn of(val: Local) -> Tag;
 
+    /// Indicates if the value type is `Undefined`.
     #[link_name = "NeonSys_Tag_IsUndefined"]
     pub fn is_undefined(val: Local) -> bool;
 
+    /// Indicates if the value type is `Null`.
     #[link_name = "NeonSys_Tag_IsNull"]
     pub fn is_null(val: Local) -> bool;
 
+    /// Indicates if the value type is `Integer`.
     #[link_name = "NeonSys_Tag_IsInteger"]
     pub fn is_integer(val: Local) -> bool;
 
+    /// Indicates if the value type is `Number`.
     #[link_name = "NeonSys_Tag_IsNumber"]
     pub fn is_number(val: Local) -> bool;
 
+    /// Indicates if the value type is `Boolean`.
     #[link_name = "NeonSys_Tag_IsBoolean"]
     pub fn is_boolean(val: Local) -> bool;
 
+    /// Indicates if the value type is `String`.
     #[link_name = "NeonSys_Tag_IsString"]
     pub fn is_string(val: Local) -> bool;
 
+    /// Indicates if the value type is `Object`.
     #[link_name = "NeonSys_Tag_IsObject"]
     pub fn is_object(val: Local) -> bool;
 
+    /// Indicates if the value type is `Array`.
     #[link_name = "NeonSys_Tag_IsArray"]
     pub fn is_array(val: Local) -> bool;
 
+    /// Indicates if the value type is `Function`.
     #[link_name = "NeonSys_Tag_IsFunction"]
     pub fn is_function(val: Local) -> bool;
 
+    /// Indicates if the value type is `Error`.
     #[link_name = "NeonSys_Tag_IsError"]
     pub fn is_error(val: Local) -> bool;
 
+    /// Indicates if the value type is `Buffer`.
     #[link_name = "NeonSys_Tag_IsBuffer"]
     pub fn is_buffer(obj: Local) -> bool;
 
