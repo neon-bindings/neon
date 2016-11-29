3f923783898a71f0c0a1855bf46765789a0a618f
jedireza <reza@akhavan.me>
2016-11-29 14:13:19 -0800

    expand neon_sys::mem mod documentation

--- crates/neon-sys/src/mem.rs
+++ crates/neon-sys/src/mem.rs
@@ -1,7 +1,9 @@
+//! A helper function for comparing `Local` handles.
 use raw::Local;
 
 extern "system" {
 
+    /// Indicates if two `Local` handles are the same.
     #[link_name = "NeonSys_Mem_SameHandle"]
     pub fn same_handle(h1: Local, h2: Local) -> bool;
 
