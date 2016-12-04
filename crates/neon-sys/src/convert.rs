445dcfc655dc960bb3df24aef5d5f864d1701d0a
jedireza <reza@akhavan.me>
2016-12-03 17:12:36 -0800

    expand neon_sys::convert mod documentation

--- crates/neon-sys/src/convert.rs
+++ crates/neon-sys/src/convert.rs
@@ -1,10 +1,18 @@
+//! Helper functions for converting `v8::Local` values.
+
 use raw::Local;
 
 extern "system" {
 
+    /// Casts the value provided to a `v8::Object` and mutates the `out` argument provided to refer
+    /// to `v8::Local` handle of the converted value. Returns `false` if the conversion didn't
+    /// succeed.
     #[link_name = "NeonSys_Convert_ToObject"]
     pub fn to_object(out: &mut Local, value: &Local) -> bool;
 
+    /// Casts the value provided to a `v8::String` and mutates the `out` argument provided to refer
+    /// to `v8::Local` handle of the converted value. Returns `false` if the conversion didn't
+    /// succeed.
     #[link_name = "NeonSys_Convert_ToString"]
     pub fn to_string(out: &mut Local, value: Local) -> bool;
 
