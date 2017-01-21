ba16515cdf95a328f7d5e260f15900368936a051
Dave Herman <dherman@mozilla.com>
2017-01-20 23:23:14 -0800

    neon-build crate

---
+++ crates/neon-build/src/lib.rs
@@ -0,0 +1,13 @@
+use std::env;
+
+/// Set up the build environment by setting Cargo configuration variables.
+pub fn setup() {
+    if cfg!(windows) {
+        let debug = env::var("DEBUG").ok().map_or(false, |s| s == "true");
+        let configuration = if debug { "Debug" } else { "Release" };
+        let node_root_dir = env::var("DEP_NEON_SYS_NODE_ROOT_DIR").unwrap();
+        let node_lib_file = env::var("DEP_NEON_SYS_NODE_LIB_FILE").unwrap();
+        println!("cargo:rustc-link-search={}\\{}", node_root_dir, configuration);
+        println!("cargo:rustc-link-lib={}", node_lib_file);
+    }
+}
