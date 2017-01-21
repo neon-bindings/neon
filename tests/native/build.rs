210cc4427c7b47f16a1f289bdfde07b006101b3e
Dave Herman <dherman@mozilla.com>
2017-01-21 07:16:22 -0800

    use neon-build in tests

--- tests/native/build.rs
+++ tests/native/build.rs
@@ -1,11 +1,5 @@
-use std::env;
+extern crate neon_build;
 
 fn main() {
-    if cfg!(windows) {
-        let debug = env::var("DEBUG").ok().map_or(false, |s| s == "true");
-        let configuration = if debug { "Debug" } else { "Release" };
-        let node_root_dir = env::var("DEP_NEON_SYS_NODE_ROOT_DIR").unwrap();
-        println!("cargo:rustc-link-lib=node");
-        println!("cargo:rustc-link-search={}\\{}", node_root_dir, configuration);
-    }
+    neon_build::setup(); // must be called in build.rs
 }
