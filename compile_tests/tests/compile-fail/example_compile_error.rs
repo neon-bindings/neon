302c5b78bad4f0277698d0fc6f311b1ee8e0c2a2
David Herman <david.herman@gmail.com>
2017-08-24 11:06:18 -0700

    fix compile-tests

--- compile_tests/tests/compile-fail/example_compile_error.rs
+++ compile_tests/tests/compile-fail/example_compile_error.rs
@@ -4,7 +4,10 @@ use neon::js::JsInteger;
 
 fn main() {
     JsInteger::new(scope, "9000")
-    //~^ ERROR unresolved name
-    //~| ERROR mismatched types
-    //~| ERROR mismatched types
+    //~^ ERROR E0425
+    //     (cannot find value `scope` in this scope)
+    //~| ERROR E0308
+    //     (mismatched types)
+    //~| ERROR E0308
+    //     (mismatched types)
 }
