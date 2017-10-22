9e5ee0605d74c59babca506dad15f7bdb61bdf5e
David Herman <david.herman@gmail.com>
2017-10-22 11:25:52 -0700

    clear build error for debug builds, which currently don't work

--- build.rs
+++ build.rs
@@ -5,5 +5,8 @@ fn main() {
         println!("cargo:node_root_dir={}", env::var("DEP_NEON_NODE_ROOT_DIR").unwrap());
         println!("cargo:node_arch={}", env::var("DEP_NEON_NODE_ARCH").unwrap());
         println!("cargo:node_lib_file={}", env::var("DEP_NEON_NODE_LIB_FILE").unwrap());
+        if let Ok(profile) = env::var("PROFILE") {
+            println!("cargo:rustc-cfg=neon_profile={:?}", profile);
+        }
     }
 }
