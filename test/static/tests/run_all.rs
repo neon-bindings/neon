4bac6cf19d89d26569ff81760c9d56c291e29825
David Herman <david.herman@gmail.com>
2017-10-22 23:24:00 -0700

    give compiletest_rs different rustc flags depending on the build profile

--- test/static/tests/run_all.rs
+++ test/static/tests/run_all.rs
@@ -1,14 +1,16 @@
 extern crate compiletest_rs as compiletest;
+extern crate static_tests;
 
 use std::path::PathBuf;
 use std::env::var;
+use static_tests::PROFILE;
 
 fn run_mode(mode: &'static str) {
     let mut config = compiletest::Config::default();
 
     let cfg_mode = mode.parse().expect("Invalid mode");
 
-    config.target_rustcflags = Some("-L target/debug/ -L target/debug/deps/".to_string());
+    config.target_rustcflags = Some(format!("-L target/{}/ -L target/{}/deps/", PROFILE, PROFILE));
     if let Ok(name) = var("TESTNAME") {
         let s : String = name.to_owned();
         config.filter = Some(s)
@@ -20,6 +22,6 @@ fn run_mode(mode: &'static str) {
 }
 
 #[test]
-fn compile_test() {
+fn run_all() {
     run_mode("compile-fail");
 }
