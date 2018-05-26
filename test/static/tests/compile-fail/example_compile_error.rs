49c9d506aa249c3c4d1b24e08fd65317f92ee38c
David Herman <david.herman@gmail.com>
2018-05-25 21:36:34 -0700

    all tests pass \o/

--- test/static/tests/compile-fail/example_compile_error.rs
+++ test/static/tests/compile-fail/example_compile_error.rs
@@ -3,11 +3,11 @@ extern crate neon;
 use neon::js::JsNumber;
 
 fn main() {
-    JsNumber::new(scope, "9000")
+    JsNumber::new(cx, "9000")
     //~^ ERROR E0425
-    //     (cannot find value `scope` in this scope)
-    //~| ERROR E0308
-    //     (mismatched types)
+    //     (cannot find value `cx` in this scope)
+    //~| ERROR E0277
+    //     (trait bound f64: From<&str> is not satisfied)
     //~| ERROR E0308
     //     (mismatched types)
 }
