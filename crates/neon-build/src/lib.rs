5f551e88eb9e99ccc03fe07edc6b20b1953565ff
K.J. Valencik <kjvalencik@gmail.com>
2019-09-06 13:21:49 -0700

    Fix electron on windows and some toolchains in linux

--- crates/neon-build/src/lib.rs
+++ crates/neon-build/src/lib.rs
@@ -15,5 +15,14 @@ pub fn setup() {
         println!("cargo:rustc-link-search={}\\{}", node_root_dir, configuration);
         println!("cargo:rustc-link-search=native={}", &node_lib_path.display());
         println!("cargo:rustc-link-lib={}", &node_lib_file_path.file_stem().unwrap().to_str().unwrap());
+
+        // Link `win_delay_load_hook.obj` for windows electron
+        let node_runtime_env = "npm_config_runtime";
+        println!("cargo:rerun-if-env-changed={}", node_runtime_env);
+        if env::var(node_runtime_env).map(|s| s == "electron") == Ok(true) {
+            println!("cargo:rustc-cdylib-link-arg=win_delay_load_hook.obj");
+            println!("cargo:rustc-cdylib-link-arg=delayimp.lib");
+            println!("cargo:rustc-cdylib-link-arg=/DELAYLOAD:node.exe");
+        }
     }
 }
