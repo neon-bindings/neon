8946d3e23b30eb77ebf1a501c2cb78b881c0bca6
Igor Artamonov <igor@artamonov.ru>
2019-10-21 15:07:03 -0400

    Fix ERR_CHILD_PROCESS_STDIO_MAXBUFFER error

--- cli/src/project.ts
+++ cli/src/project.ts
@@ -26,9 +26,8 @@ export default class Project {
 
   static async create(root: string, options: Partial<ProjectOptions> = {}): Promise<Project> {
     let { crate = 'native' } = options;
-    const { stdout } = await execFile("cargo", ["metadata", "--format-version=1"], {
-      cwd: path.join(root, crate),
-      maxBuffer: 1024 * 1024
+    const { stdout } = await execFile("cargo", ["metadata", "--format-version=1", "--no-deps"], {
+      cwd: path.join(root, crate)
     });
     const targetDirectory: string = JSON.parse(stdout).target_directory;
 
