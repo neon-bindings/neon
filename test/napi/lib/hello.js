56baef81fe70398eca2e9c1a353ef11d9b0b9ca3
Renée Kooi <renee@kooi.me>
2020-01-25 17:31:15 +0100

    fix `undefined` test

--- test/napi/lib/hello.js
+++ test/napi/lib/hello.js
@@ -9,6 +9,7 @@ describe('hello', function() {
 
   it('should export global singletons for JS primitives', function () {
     assert.equal(addon.undefined, undefined);
+    assert.ok(addon.hasOwnProperty('undefined'));
     assert.equal(addon.null, null);
     assert.equal(addon.true, true);
     assert.equal(addon.false, false);
