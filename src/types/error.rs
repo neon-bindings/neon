d18c514b4d06b0686f32d49d3deab16c949c1e12
Anshul Goyal <anshulgoel151999@gmail.com>
2020-03-27 02:50:15 +0530

    Fixed error

--- src/types/error.rs
+++ src/types/error.rs
@@ -37,16 +37,6 @@ impl Object for JsError { }
 
 impl JsError {
     /// Creates a direct instance of the [`Error`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Error) class.
-    #[cfg(feature = "legacy-runtime")]
-    pub fn error<'a, C: Context<'a>, S: AsRef<str>>(cx: &mut C, msg: S) -> NeonResult<Handle<'a, JsError>> {
-        let msg = cx.string(msg.as_ref());
-        build(|out| unsafe {
-            neon_runtime::error::new_error(out, msg.to_raw());
-            true
-        })
-    }
-
-    #[cfg(feature = "napi-runtime")]
     pub fn error<'a, C: Context<'a>, S: AsRef<str>>(cx: &mut C, msg: S) -> NeonResult<Handle<'a, JsError>> {
         #[cfg(feature = "legacy-runtime")]
         let msg = cx.string(msg.as_ref());
