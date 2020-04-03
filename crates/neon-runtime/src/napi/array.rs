a4c5a3c831000e4ed70156c0276c102fd84efed4
Renée Kooi <renee@kooi.me>
2020-04-03 13:16:29 +0200

    Document panics for `neon_runtime::napi::array::len`

--- crates/neon-runtime/src/napi/array.rs
+++ crates/neon-runtime/src/napi/array.rs
@@ -7,6 +7,10 @@ use nodejs_sys as napi;
 pub unsafe extern "C" fn new(_out: &mut Local, _env: Env, _length: u32) { unimplemented!() }
 
 /// Gets the length of a `napi_value` containing a JavaScript Array.
+///
+/// # Panics
+/// This function panics if `array` is not an Array, or if a previous n-api call caused a pending
+/// exception.
 pub unsafe extern "C" fn len(env: Env, array: Local) -> u32 {
     let mut len = 0;
     assert_eq!(napi::napi_get_array_length(env, array, &mut len as *mut _), napi::napi_status::napi_ok);
