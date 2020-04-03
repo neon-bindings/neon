ff8a4f2606c692c73785d2153b41c66ef228f633
Renée Kooi <renee@kooi.me>
2020-04-03 13:20:53 +0200

    Document out param in `neon_runtime::nan::object::set_{index,string}`.

--- crates/neon-runtime/src/napi/object.rs
+++ crates/neon-runtime/src/napi/object.rs
@@ -15,7 +15,9 @@ pub unsafe extern "C" fn get_own_property_names(out: &mut Local, env: Env, objec
 }
 
 // Unused.
-pub unsafe extern "C" fn get_isolate(_obj: Local) -> Env { unimplemented!() }
+pub unsafe extern "C" fn get_isolate(_obj: Local) -> Env {
+    unimplemented!()
+}
 
 pub unsafe extern "C" fn get_index(out: &mut Local, env: Env, object: Local, index: u32) -> bool {
     let status = napi::napi_get_element(env, object, index, out as *mut _);
@@ -23,6 +25,13 @@ pub unsafe extern "C" fn get_index(out: &mut Local, env: Env, object: Local, ind
     status == napi::napi_status::napi_ok
 }
 
+/// Sets the key value of a `napi_value` at the `index` provided. Returns `true` if the set
+/// succeeded.
+///
+/// The `out` parameter and the return value contain the same information for historical reasons,
+/// see [discussion].
+///
+/// [discussion]: https://github.com/neon-bindings/neon/pull/458#discussion_r344827965
 pub unsafe extern "C" fn set_index(out: &mut bool, env: Env, object: Local, index: u32, val: Local) -> bool {
     let status = napi::napi_set_element(env, object, index, val);
     *out = status == napi::napi_status::napi_ok;
@@ -35,56 +44,43 @@ pub unsafe extern "C" fn get_string(env: Env, out: &mut Local, object: Local, ke
 
     // Not using `crate::string::new()` because it requires a _reference_ to a Local,
     // while we only have uninitialized memory.
-    if napi::napi_create_string_utf8(
-        env,
-        key as *const i8,
-        len as usize,
-        key_val.as_mut_ptr(),
-    ) != napi::napi_status::napi_ok {
+    if napi::napi_create_string_utf8(env, key as *const i8, len as usize, key_val.as_mut_ptr())
+        != napi::napi_status::napi_ok
+    {
         return false;
     }
 
     // Not using napi_get_named_property() because the `key` may not be null terminated.
-    if napi::napi_get_property(
-        env,
-        object,
-        key_val.assume_init(),
-        out as *mut _,
-    ) != napi::napi_status::napi_ok {
+    if napi::napi_get_property(env, object, key_val.assume_init(), out as *mut _)
+        != napi::napi_status::napi_ok
+    {
         return false;
     }
 
     true
 }
 
-pub unsafe extern "C" fn set_string(
-    env: Env,
-    out: &mut bool,
-    object: Local,
-    key: *const u8,
-    len: i32,
-    val: Local,
-) -> bool {
+/// Sets the key value of a `napi_value` at a named key. Returns `true` if the set succeeded.
+///
+/// The `out` parameter and the return value contain the same information for historical reasons,
+/// see [discussion].
+///
+/// [discussion]: https://github.com/neon-bindings/neon/pull/458#discussion_r344827965
+pub unsafe extern "C" fn set_string(env: Env, out: &mut bool, object: Local, key: *const u8, len: i32, val: Local) -> bool {
     let mut key_val = MaybeUninit::uninit();
 
     *out = true;
 
-    if napi::napi_create_string_utf8(
-        env,
-        key as *const i8,
-        len as usize,
-        key_val.as_mut_ptr(),
-    ) != napi::napi_status::napi_ok {
+    if napi::napi_create_string_utf8(env, key as *const i8, len as usize, key_val.as_mut_ptr())
+        != napi::napi_status::napi_ok
+    {
         *out = false;
         return false;
     }
 
-    if napi::napi_set_property(
-        env,
-        object,
-        key_val.assume_init(),
-        val,
-    ) != napi::napi_status::napi_ok {
+    if napi::napi_set_property(env, object, key_val.assume_init(), val)
+        != napi::napi_status::napi_ok
+    {
         *out = false;
         return false;
     }
@@ -98,6 +94,12 @@ pub unsafe extern "C" fn get(out: &mut Local, env: Env, object: Local, key: Loca
     status == napi::napi_status::napi_ok
 }
 
+/// Sets the property value of an `napi_value` object, named by another `napi_value` `key`. Returns `true` if the set succeeded.
+///
+/// The `out` parameter and the return value contain the same information for historical reasons,
+/// see [discussion].
+///
+/// [discussion]: https://github.com/neon-bindings/neon/pull/458#discussion_r344827965
 pub unsafe extern "C" fn set(out: &mut bool, env: Env, object: Local, key: Local, val: Local) -> bool {
     let status = napi::napi_set_property(env, object, key, val);
     *out = status == napi::napi_status::napi_ok;
