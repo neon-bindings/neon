e8a53f44e92c9f929aa8e828fbbcf0e225784c61
Renée Kooi <renee@kooi.me>
2020-07-10 12:59:20 +0200

    n-api: some warning cleanup

--- crates/neon-runtime/src/napi/call.rs
+++ crates/neon-runtime/src/napi/call.rs
@@ -23,7 +23,7 @@ pub unsafe extern "C" fn set_return(_info: FunctionCallbackInfo, _value: Local) 
 
 }
 
-pub unsafe extern "C" fn get_isolate(info: FunctionCallbackInfo) -> Env { unimplemented!() }
+pub unsafe extern "C" fn get_isolate(_info: FunctionCallbackInfo) -> Env { unimplemented!() }
 
 // FIXME: Remove. This will never be implemented
 pub unsafe extern "C" fn current_isolate() -> Env { panic!("current_isolate won't be implemented in n-api") }
