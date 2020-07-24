5a63267ee8072a06d1807b4a76019e7a45b6cfa2
Renée Kooi <renee@kooi.me>
2020-07-24 19:09:04 +0200

    napi: port array tests from nan

---
+++ test/napi/lib/arrays.js
@@ -0,0 +1,16 @@
+var addon = require('../native');
+var assert = require('chai').assert;
+
+describe('JsArray', function() {
+  it('return a JsArray built in Rust', function () {
+    assert.deepEqual([], addon.return_js_array());
+  });
+
+  it('return a JsArray with a number at index 0', function () {
+    assert.deepEqual([9000], addon.return_js_array_with_number());
+  });
+
+  it('return a JsArray with an string at index 0', function () {
+    assert.deepEqual(["hello node"], addon.return_js_array_with_string());
+  });
+});
