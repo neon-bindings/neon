3e4e52af6ca84d004ad6a59400c73cafa10164f8
David Herman <david.herman@gmail.com>
2021-09-20 11:25:09 -0700

    Simpler doctest fix, per @kjvalencik's review suggestion

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -39,18 +39,15 @@
 //! a [`DowncastError`](crate::handle::DowncastError):
 //!
 //! ```
+//! # #[cfg(feature = "napi-1")] {
 //! # use neon::prelude::*;
 //! fn as_array<'a>(
 //!     cx: &mut impl Context<'a>,
 //!     object: Handle<'a, JsObject>
 //! ) -> JsResult<'a, JsArray> {
-//! #   #[cfg(feature = "legacy-runtime")]
-//! #   return object.downcast().or_throw(cx);
-//! #   #[cfg(feature = "napi-1")]
-//! #   return
 //!     object.downcast(cx).or_throw(cx)
-//! #   ;
 //! }
+//! # }
 //! ```
 //!
 //! ### The JavaScript Type Hierarchy
