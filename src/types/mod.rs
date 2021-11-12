5e16313e6c374dc4c9ef3c4df7eeef6cbcc441d9
K.J. Valencik <kjvalencik@gmail.com>
2021-11-12 12:53:43 -0500

    Add comment to `ptr::null_mut()` call back

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -773,7 +773,12 @@ impl JsFunction {
             FunctionContext::with(env, &info, |cx| {
                 convert_panics(env, AssertUnwindSafe(|| f(cx)))
                     .map(|v| v.to_raw())
-                    .unwrap_or_else(|_| ptr::null_mut())
+                    // We do not have a Js Value to return, most likely due to an exception.
+                    // If we are in a throwing state, constructing a Js Value would be invalid.
+                    // While not explicitly written, the Node-API documentation includes many examples
+                    // of returning `NULL` when a native function does not return a value.
+                    // https://nodejs.org/api/n-api.html#n_api_napi_create_function
+                    .unwrap_or_else(|_: Throw| ptr::null_mut())
             })
         };
 
