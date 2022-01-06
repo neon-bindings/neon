b7b80dfe64899f0fc69c2d3a6b23c895837e95db
deepanshu <deepanshuyadav.04@gmail.com>
2022-01-06 13:10:43 +0530

    create-neon now deletes the partial project directory on non-zero exit code

--- create-neon/src/shell.ts
+++ create-neon/src/shell.ts
@@ -1,11 +1,15 @@
-import { spawn } from 'child_process';
-
+import { spawn } from "child_process";
+import fs from "fs/promises";
 /**
  * Transparently shell out to an executable with a list of arguments.
  * All stdio is inherited directly from the current process.
  */
-export default function shell(cmd: string, args: string[], cwd: string): Promise<undefined> {
-  let child = spawn(cmd, args, { stdio: 'inherit', shell: true, cwd });
+export default function shell(
+  cmd: string,
+  args: string[],
+  cwd: string
+): Promise<undefined> {
+  let child = spawn(cmd, args, { stdio: "inherit", shell: true, cwd });
 
   let resolve: (result: undefined) => void;
   let reject: (error: Error) => void;
@@ -15,19 +19,38 @@ export default function shell(cmd: string, args: string[], cwd: string): Promise
     reject = rej;
   });
 
-  child.on('exit', (code) => {
+  function deleteNeonDir(): Promise<void> {
+    return fs.rm(cwd, { force: true, recursive: true });
+  }
+
+  child.on("exit", async (code) => {
     if (code == null) {
+      await deleteNeonDir();
       process.exit(1);
     }
     if (code !== 0) {
+      await deleteNeonDir();
       process.exit(code);
+    }
+
+    if (code === 0) {
+      try {
+          let data = await fs.readFile(`${cwd}/package.json`,'utf8')
+          let { description, author, license } = JSON.parse(data);
+          if ([description, author, license].includes(undefined)) {
+               throw new Error("Ctrl+C pressed");
+               }
+        } catch (e) {
+           await deleteNeonDir();
+           process.exit();
+      }
     }
     resolve(undefined);
   });
 
-  child.on('error', (error) => {
+  child.on("error", async (error) => {
+    await deleteNeonDir();
     reject(error);
   });
-
   return result;
 }
