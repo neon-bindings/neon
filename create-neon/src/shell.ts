0dd0d3789ad1646c3b0a8cdb8cf4ab8aa310ab7d
deepanshu <deepanshuyadav.04@gmail.com>
2022-01-11 17:08:14 +0530

    should be working

--- create-neon/src/shell.ts
+++ create-neon/src/shell.ts
@@ -1,5 +1,7 @@
 import { spawn } from "child_process";
-import fs from "fs/promises";
+import {promises as fs} from "fs";
+import path from 'path';
+
 /**
  * Transparently shell out to an executable with a list of arguments.
  * All stdio is inherited directly from the current process.
@@ -19,38 +21,30 @@ export default function shell(
     reject = rej;
   });
 
-  function deleteNeonDir(): Promise<void> {
-    return fs.rm(cwd, { force: true, recursive: true });
-  }
-
   child.on("exit", async (code) => {
     if (code == null) {
-      await deleteNeonDir();
-      process.exit(1);
+	reject(Error(`error code: ${code}`))
     }
     if (code !== 0) {
-      await deleteNeonDir();
-      process.exit(code);
+	reject(Error(`error code: ${code}`))
     }
 
     if (code === 0) {
       try {
-          let data = await fs.readFile(`${cwd}/package.json`,'utf8')
+          let data = await fs.readFile(path.join(cwd,'package.json'),'utf8')
           let { description, author, license } = JSON.parse(data);
           if ([description, author, license].includes(undefined)) {
-               throw new Error("Ctrl+C pressed");
+        	reject(Error(`error code: ${code}`))
                }
         } catch (e) {
-           await deleteNeonDir();
-           process.exit();
+              reject(e)
       }
     }
     resolve(undefined);
   });
 
   child.on("error", async (error) => {
-    await deleteNeonDir();
-    reject(error);
+      reject(error);
   });
   return result;
 }
