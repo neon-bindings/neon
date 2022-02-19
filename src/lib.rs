499142226bf1b81a3c854730cb17a2b97b2b521c
MikaelUrankar <mikael.urankar@gmail.com>
2022-02-19 09:22:03 +0100

    Add FreeBSD

--- src/lib.rs
+++ src/lib.rs
@@ -173,6 +173,7 @@ macro_rules! register_module {
     ($module:pat, $init:block) => {
         // Mark this function as a global constructor (like C++).
         #[allow(improper_ctypes)]
+        #[cfg_attr(target_os = "freebsd", link_section = ".ctors")]
         #[cfg_attr(target_os = "linux", link_section = ".ctors")]
         #[cfg_attr(target_os = "android", link_section = ".ctors")]
         #[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
