4e350ea1d74cc90158e7bc3b8ce273bf9b6a5c0f
K.J. Valencik <kjvalencik@gmail.com>
2022-05-23 09:35:25 -0400

    soundness(neon): Require `T: 'static` on `JsArrayBuffer::external` and `JsBuffer::external`
    Fixes https://github.com/neon-bindings/neon/issues/896

--- src/types/buffer/types.rs
+++ src/types/buffer/types.rs
@@ -43,7 +43,7 @@ impl JsBuffer {
     pub fn external<'a, C, T>(cx: &mut C, data: T) -> Handle<'a, Self>
     where
         C: Context<'a>,
-        T: AsMut<[u8]> + Send,
+        T: AsMut<[u8]> + Send + 'static,
     {
         let env = cx.env().to_raw();
         let value = unsafe { neon_runtime::buffer::new_external(env, data) };
@@ -151,7 +151,7 @@ impl JsArrayBuffer {
     pub fn external<'a, C, T>(cx: &mut C, data: T) -> Handle<'a, Self>
     where
         C: Context<'a>,
-        T: AsMut<[u8]> + Send,
+        T: AsMut<[u8]> + Send + 'static,
     {
         let env = cx.env().to_raw();
         let value = unsafe { neon_runtime::arraybuffer::new_external(env, data) };
