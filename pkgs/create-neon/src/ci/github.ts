8de9b1432e79295a210287858cea7c2cf8a68895
David Herman <david.herman@gmail.com>
2024-04-14 23:38:35 -0700

    remove comments.yml.hbs from templates

--- pkgs/create-neon/src/ci/github.ts
+++ pkgs/create-neon/src/ci/github.ts
@@ -6,7 +6,6 @@ const TEMPLATES: Record<string, string> = {
   "setup.yml.hbs": path.join(".github", "actions", "setup", "action.yml"),
   ".env.hbs": path.join(".github", ".env"),
   "build.yml.hbs": path.join(".github", "workflows", "build.yml"),
-  "comments.yml.hbs": path.join(".github", "workflows", "comments.yml"),
   "release.yml.hbs": path.join(".github", "workflows", "release.yml"),
   "test.yml.hbs": path.join(".github", "workflows", "test.yml"),
 };
