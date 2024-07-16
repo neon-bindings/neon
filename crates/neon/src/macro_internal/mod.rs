5b6f345145787b5308d14586cd6999f09b688116
K.J. Valencik <kjvalencik@gmail.com>
2024-07-16 12:14:11 -0400

    improve(neon): Use autoref specialization for JSON wrapping return values in exported functions
    https://github.com/dtolnay/case-studies/blob/master/autoref-specialization/README.md

--- crates/neon/src/macro_internal/mod.rs
+++ crates/neon/src/macro_internal/mod.rs
@@ -2,7 +2,12 @@
 
 pub use linkme;
 
-use crate::{context::ModuleContext, handle::Handle, result::NeonResult, types::JsValue};
+use crate::{
+    context::{Context, ModuleContext},
+    handle::Handle,
+    result::{JsResult, NeonResult},
+    types::{extract::TryIntoJs, JsValue},
+};
 
 type Export<'cx> = (&'static str, Handle<'cx, JsValue>);
 
@@ -11,3 +16,60 @@ pub static EXPORTS: [for<'cx> fn(&mut ModuleContext<'cx>) -> NeonResult<Export<'
 
 #[linkme::distributed_slice]
 pub static MAIN: [for<'cx> fn(ModuleContext<'cx>) -> NeonResult<()>];
+
+// Provides an identically named method to `NeonExportReturnJson` for easy swapping in macros
+pub trait NeonExportReturnValue<'cx> {
+    fn try_neon_export_return<C>(self, cx: &mut C) -> JsResult<'cx, JsValue>
+    where
+        C: Context<'cx>;
+}
+
+impl<'cx, T> NeonExportReturnValue<'cx> for T
+where
+    T: TryIntoJs<'cx>,
+{
+    fn try_neon_export_return<C>(self, cx: &mut C) -> JsResult<'cx, JsValue>
+    where
+        C: Context<'cx>,
+    {
+        self.try_into_js(cx).map(|v| v.upcast())
+    }
+}
+
+#[cfg(feature = "serde")]
+// Trait used for specializing `Json` wrapping of `T` or `Result<T, _>` in macros
+// Leverages the [autoref specialization](https://github.com/dtolnay/case-studies/blob/master/autoref-specialization/README.md) technique
+pub trait NeonExportReturnJson<'cx> {
+    fn try_neon_export_return<C>(self, cx: &mut C) -> JsResult<'cx, JsValue>
+    where
+        C: Context<'cx>;
+}
+
+#[cfg(feature = "serde")]
+// More specific behavior wraps `Result::Ok` in `Json`
+impl<'cx, T, E> NeonExportReturnJson<'cx> for Result<T, E>
+where
+    T: serde::Serialize,
+    E: TryIntoJs<'cx>,
+{
+    fn try_neon_export_return<C>(self, cx: &mut C) -> JsResult<'cx, JsValue>
+    where
+        C: Context<'cx>,
+    {
+        self.map(crate::types::extract::Json).try_into_js(cx)
+    }
+}
+
+#[cfg(feature = "serde")]
+// Due to autoref behavior, this is less specific than the other implementation
+impl<'cx, T> NeonExportReturnJson<'cx> for &T
+where
+    T: serde::Serialize,
+{
+    fn try_neon_export_return<C>(self, cx: &mut C) -> JsResult<'cx, JsValue>
+    where
+        C: Context<'cx>,
+    {
+        crate::types::extract::Json(self).try_into_js(cx)
+    }
+}
