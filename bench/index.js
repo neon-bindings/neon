77f7a7b2f39237fa9d8b32249fc8e3ca9d22b03f
David Herman <david.herman@gmail.com>
2025-03-13 11:22:33 -0700

    Add latency value to benchmark reporting

--- bench/index.js
+++ bench/index.js
@@ -1,17 +1,34 @@
 const { Suite, jsonReport } = require("bench-node");
 const addon = require("./index.node");
 
+function median(values) {
+  const sorted = [...values].sort((a, b) => a - b);
+  const n = sorted.length;
+  return n % 2 === 0
+    ? (sorted[n / 2 - 1] + sorted[n / 2]) / 2
+    : sorted[Math.floor(n / 2)];
+}
+
 // A custom reporter for the bencher.dev benchmarking platform.
 // Format: https://bencher.dev/docs/reference/bencher-metric-format/
+//
+// The reporter provides two measures for each benchmark:
+// - "throughput": The number of operations per second.
+// - "latency": The time taken to perform an operation, in ns.
+//   * "value": The median value of all samples.
+//   * "lower_value": The minimum value of all samples.
+//   * "upper_value": The maximum value of all samples.
 function reportBencherDev(results) {
   const bmf = Object.create(null);
   for (const result of results) {
-    // If https://github.com/RafaelGSS/bench-node/issues/66 is fixed, then we can use
-    // result.histogram to report a "latency" measure with the median as the "value",
-    // min as "lower_value", and max as "upper_value".
     bmf[result.name] = {
       throughput: {
         value: result.opsSec,
+      },
+      latency: {
+        value: median(result.histogram.sampleData),
+        lower_value: result.histogram.min,
+        upper_value: result.histogram.max,
       },
     };
   }
